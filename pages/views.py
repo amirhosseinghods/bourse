6467990af482053b2bedd8e99531fa98ca7604ca
Sepehr akbarzadeh <sa.goldeneagle@gmail.com>
2020-04-10 22:31:37 -0700

    social media is added. text banner is fixed.

--- pages/views.py
+++ pages/views.py
@@ -41,13 +41,28 @@ class HomeView(CreateView):
 
 class ContactView(CreateView):
     template_name = 'pages/contact.html'
+    model = Contact
     page_name = _('Contact Us')
-    is_rtl = True
+    success_url = reverse_lazy('pages:success')
+    is_rtl = False
+
+    def form_valid(self, form):
+        self.object = form.save()
+        send_mail(
+            'Smart Bourse Contact: {}'.format(form.data['subject']),
+            '{}'.format(form.data['message']),
+            'animatedidea@gmail.com',
+            ['sa.goldeneagle@gmail.com'],
+            fail_silently=False,
+        )
+        return HttpResponseRedirect(self.get_success_url())
 
     def get_context_data(self, **kwargs):
         context = super(ContactView, self).get_context_data(**kwargs)
 
         return context
+
+
 
 class SuccessView(TemplateView):
     template_name = "pages/success.html"

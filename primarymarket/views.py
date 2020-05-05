4732578c3ab9c38ec4a0619b9494ed53533dda98
sepehr.ak <sa.goideneagle@gmail.com>
2020-05-05 16:15:32 +0430

    primary market ugettext lazy forgotten line

--- primarymarket/views.py
+++ primarymarket/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import PrimaryMarket
-
+from django.utils.translation import ugettext_lazy as _
 
 class PrimaryMarketListView(ListView):
     model = PrimaryMarket

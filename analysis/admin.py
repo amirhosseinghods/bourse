cd0af459168494182a615740de2a61a56c041f0f
sepehr.ak <sa.goideneagle@gmail.com>
2020-05-05 12:09:09 +0430

    analyse post changed

--- analysis/admin.py
+++ analysis/admin.py
@@ -9,8 +9,9 @@ from .models import AnalyzeComment
 class AnalyzePostAdmin(admin.ModelAdmin):
     '''Admin View for Analyze Post'''
 
-    list_display = ('slug', 'author', 'category', 'created')
+    list_display = ('slug', 'author', 'is_shown', 'important', 'category', 'created')
     list_filter = ('author', 'category',)
+    list_editable = ('is_shown', 'important', 'category')
     filter_horizontal = ('tags',)
     search_fields = ('title',)  
     ordering = ('-created',)
